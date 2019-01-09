b2c68f106871f82797c133f8a3e01992a53bbe89
RobbaB14 <barbara.robba@gmail.com>
2019-01-09 13:55:01 +0100

    popravki

--- A_star.py
+++ A_star.py
@@ -1,51 +1,43 @@
-# alogirtem A*
-# G naj bo obtežen usmerjen graf (z n vozlišči in m povezavami)
-# podan kot seznam n seznamov:
-    # za vsako vozlišče i podamo seznam parov (j, k), kjer je
-    # i vozlišče v katerem se poezava začne
-    # j vozlišče v katerem se povezava konča
-    # k utež na povezavi med i in j.
-# h je heveristična funkcija, ki deluje na vozliščih. Podana je v obliki
-# seznama H = [h(0), h(1), h(2), ...h(n)]
-# a je številka vozlišča, v katerem začnemo iskanje optimalne poti
-# b je številka vozlišča v katerem želimo končati pot.
+# algorithm A*
+# G is weighted graph (V(G)=n and E(G)=m) given as list.
+# G[i] is list of tuples (j,k) denoting e=(i,j) with cost k
+# h is heuristic function given as list H = [h(0), h(1),...,h(n)]
+# a is first node of path
+# b is last node of path
 
 import heapq
 
-def A_star(G, a, b, H):
-    seznam = []
-    pot = []
-    for i in range(len(G)):
-        pot.append(0)
-    pot[a] = a
-    cena_poti = []
+def A_star(graph, a, b, H):
+    nodes = []
+    path = []
+    for i in range(len(graph)):
+        path.append(0)
+    path[a] = a
+    cost = []
 
-    for i in range(len(G)):
+    for i in range(len(graph)):
         if i == a:
-            seznam.append((0,a))
-            cena_poti.append(0)
+            nodes.append((0,a))
+            cost.append(0)
         else:
-            seznam.append((2**16, i)) #razdalije nastavimo na 'neskončno'.
-            cena_poti.append(2**16)
-    # print(seznam)
-    # print(cena_poti)
-    heapq.heapify(seznam) # naredimo kopico.
-    while len(seznam)>0:
-        (t,v) = heapq.heappop(seznam) # najcenejše vozlišče in njegova cena
-        for (i,j) in G[v]: # i je vozlišče, j je utež na povezavi do vozlišča
-            # print(H[i])
-            if cena_poti[i] > t + j + H[i]:
-                cena_poti[i] = t + j + H[i]
-                heapq.heappush(seznam, (t+j+H[i], i))
-                pot[i] = v
+            nodes.append((2**16, i)) #distance = infinity
+            cost.append(2**16)
+    heapq.heapify(nodes)
+    while len(nodes)>0:
+        (t,v) = heapq.heappop(nodes)
+        for (i,j) in G[v]: # (i,j) = (node, cost)
+            if cost[i] > t + j + H[i]:
+                cost[i] = t + j + H[i]
+                heapq.heappush(nodes, (t+j+H[i], i))
+                path[i] = v
     c = b
-    iskana_pot = [c]
+    best_path = [c]
     while c != a:
-        iskana_pot.append(pot[c])
-        c = pot[c]
-    iskana_pot[::-1]
+        best_path.append(path[c])
+        c = path[c]
+    best_path = best_path[::-1]
 
-    return iskana_pot
+    return best_path
 G = [[(1,3),(4,3)],[],[(0,1),(1,2)],[(2,4),(0,7)],[(3,2)]]
 H = [1,6,15,3,4]
 b = 1
--- k_means.py
+++ k_means.py
@@ -1,69 +1,50 @@
 # k-menas clustering
-# D je množica točk
-# k je število klastrov, ki jih želimo.
+
+# X... (n*d) matrix
+# k... number of clusters
 
 import numpy as np
 import random
-import math
-
-def dist (p0x, p0y, p1x, p1y):
-    return math.sqrt((p0x-p1x)**2 + (p0y - p1y)**2)
-
-def my_mean(M):
-    x = 0
-    y = 0
-    n = len(M)
-    for i in range(n):
-        x+=M[i][0]
-        y+=M[i][1]
-    return([x/n, y/n])
 
 
-def k_means(D, k):
-    n = len(D) # število točk, ki so dane.
-    C = [] # centri, ki jih iščemo.
+def k_means(X, k):
+    n = len(X) # number of given points
+    d = len(X[0]) # dimension
+    centres = np.zeros((k,d))
     i = 0
-    izbrane = [] # na začetku naključno izberemo k danih točk.
-    premik = [] # ko bo premik 0, se bo zanka ustavila.
-    nic = [] # nicelni vektor s katerim primerjamo.
-    while i < k: # izberemo k paroma različnih točk iz D
+    chosen_lines = [] # we choose k points (k lines in X)
+    move = np.ones((1,k)) # vector of distance between last end new C
+    zero = np.zeros((1,k))
+    while i < k: # We choose k points from X and put them in C
         j = random.randint(0,n-1)
-        if j not in izbrane:
-            izbrane.append(j)
-            premik.append(1)
-            nic.append(0)
-            C.append(D[j])
+        if j not in chosen_lines:
+            chosen_lines.append(j)
+            centres[i]=(X[j])
             i += 1
-    while premik != nic:
-        G = [] # seznam kamor bomo shranjevali k seznamov z vozlišči,
-                # ki so najbljižje izbrani točki.
-        for i in range(k):
-            G.append([])
-        for i in range(len(D)): #klasificiramo točke v skupine
-            oddaljenost = []
-            for j in range(k):
-                d = dist(D[i][0], D[i][1], C[j][0], C[j][1])
-                oddaljenost.append(d)
-            w = oddaljenost.index(min(oddaljenost)) # s, ki je najbližje
-            G[w].append(D[i])
-        for i in range(k):
-            nov = my_mean(G[i])
-            premik[i] = dist(nov[0], nov[1], C[i][0], C[i][1])
-            C[i]= nov
-    return (C)
 
-m = 10
-D = []
-for i in range(m):
-    l = random.uniform(3, 6)
-    k = random.uniform(4, 5)
-    D.append([l,k])
-for i in range(m):
-    l = random.uniform(-5, -3)
-    k = random.uniform(-1, -4)
-    D.append([l, k])
-for i in range(m):
-    l = random.uniform(0, 2)
-    k = random.uniform(-1, 3)
-    D.append([l, k])
-print(k_means(D,3))
+    while np.linalg.norm(move-zero) != 0:
+        partition = [] # current partition
+        for i in range(k):
+            partition.append([])
+        for i in range(len(X)):
+            distance = []
+            for j in range(k):
+                distance.append(np.linalg.norm(X[i]-centres[j]))
+            nearest_c = distance.index(min(distance))
+            partition[nearest_c].append(X[i])
+        for i in range(k):
+            new_s = np.mean(partition[i], axis=0)
+            move[0][i] = np.linalg.norm(new_s-centres[i])
+            centres[i]= new_s
+    return (centres)
+
+# example of input:
+m = 4
+d = 4
+Y_1 = 2*np.random.rand(m,d)+[-2,-2,0,0]
+X = Y_1
+Y_2 = np.random.rand(m,d)+ [0,0,1,1]
+X = np.r_[X,Y_2]
+Y_3 = np.random.rand(m,d) + [0,3,3,0]
+X = np.r_[X,Y_3]
+print(k_means(X,3))
